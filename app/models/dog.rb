f6c638a0ccf040f2a0193dd9e3d871b69df6ff03
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-25 06:49:21 +0000

    automatically backed up by learn

---
+++ app/models/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+  attr_accessor :name, :breed, :age
+
+  @@all = []
+
+  def initialize(name: nil, breed: nil, age: nil)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+
+end
